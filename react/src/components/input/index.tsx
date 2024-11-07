2c481d15c2913e89e0c353a0b00c0bde68d1980d
Jessica Castro <jessica.castro@ifood.com.br>
2024-11-07 00:02:12 -0300

    react: rename some files

--- react/src/components/input/index.tsx
+++ react/src/components/input/index.tsx
@@ -1,5 +1,5 @@
-import { ErrorMessage } from "./errorMessage";
-import { FormField } from "./formField";
+import { ErrorMessage } from "./error-message";
+import { FormField } from "./form-field";
 import { Icon } from "./icon";
 import { Label } from "./label";
 import { Root } from "./root";
